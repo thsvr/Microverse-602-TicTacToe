c1e950bb1cc650f4110944989af743eedde7005c
sarah <sarachamorro@gmail.com>
2020-03-05 21:50:50 -0300

    Fix more linting errors

--- test-helpers/index.js
+++ test-helpers/index.js
@@ -1,5 +1,5 @@
 const sameContentArr = (a1, a2) => {
-  if (a1.length !== a2.length) { return false }
+  if (a1.length !== a2.length) { return false; }
 
   return a1.every((n, i) => n === a2[i]);
 };
